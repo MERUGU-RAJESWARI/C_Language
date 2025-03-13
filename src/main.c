d80e161fc5dd8d7de9dc49be114f080a1d234476
MERUGU-RAJESWARI <139354752+MERUGU-RAJESWARI@users.noreply.github.com>
2025-03-13 09:40:35 +0000

    rajeswari123

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "functions.h"
+
+int main() {
+    int x = 5, y = 10;
+    printf("Addition: %d\n", add(x, y));
+    printf("Multiplication: %d\n", multiply(x, y));
+    return 0;
+}
